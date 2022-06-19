29ee676ec14e07507322c5c894e54b7ad1ad0eaf
LifelongS <96198844+LifelongS@users.noreply.github.com>
2022-06-19 14:55:33 +0300

    Add files via upload

---
+++ 3.sql
@@ -0,0 +1,7 @@
+--Отобразить все месяцы, где общая сумма продаж превысила 10 000.
+SELECT CAST(YEAR(i.InvoiceDate) AS VARCHAR(4)) + '-' + CAST(MONTH(i.InvoiceDate) AS VARCHAR(2)) AS [MonthNumber]
+FROM Sales.InvoiceLines il
+     INNER JOIN Sales.Invoices i ON i.InvoiceID = il.InvoiceID
+GROUP BY YEAR(i.InvoiceDate), MONTH(i.InvoiceDate)
+HAVING SUM(il.Quantity * il.UnitPrice) > 10000
+ORDER BY YEAR(i.InvoiceDate), MONTH(i.InvoiceDate);
